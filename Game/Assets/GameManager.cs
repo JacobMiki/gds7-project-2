822dccff0e9573a98fbdf4d0192bb33663d92871
JacobMiki <JacobMiki@users.noreply.github.com>
2020-02-11 08:11:05 +0100

    feat(Game): create first prototype

---
+++ Game/Assets/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager => GameObject.FindObjectOfType<GameManager>();
+
+    public int Score { get => _score; set { _score = value; ScoreText.text = _score.ToString(); } }
+
+    public int CherryCount { get; set; }
+
+    public Text ScoreText;
+    public Text TimeText;
+    public GameObject GameOverText;
+
+    private int _score;
+    private float _gameOverTimer;
+
+    void Start()
+    {
+        Input.backButtonLeavesApp = true;
+
+    }
+
+    void Update()
+    {
+        if (CherryCount == 0)
+        {
+            GameOverText.SetActive(true);
+            _gameOverTimer += Time.deltaTime;
+
+            if (_gameOverTimer >= 5f)
+            {
+                var scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.name);
+            }
+
+            return;
+        }
+
+        var mm = Mathf.FloorToInt(Time.timeSinceLevelLoad / 60).ToString().PadLeft(2, '0');
+        var ss = Mathf.FloorToInt(Time.timeSinceLevelLoad % 60).ToString().PadLeft(2, '0');
+
+        TimeText.text = mm + ":" + ss;
+
+
+    }
+
+
+}
---
+++ Game/Assets/Scripts/Cherry.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cherry : MonoBehaviour
+{
+    private GameManager _gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().flipX = Random.value > 0.5f;
+        _gameManager = GameObject.FindObjectOfType<GameManager>();
+
+        _gameManager.CherryCount++;
+    }
+
+    void OnDestroy()
+    {
+        _gameManager.CherryCount--;
+    }
+
+}
